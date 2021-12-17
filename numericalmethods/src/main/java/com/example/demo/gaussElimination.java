bab2a634e1b2fcf5f613216bcfa885340e3b0b96
MohamedElgreatly <91938263+MohamedElgreatly@users.noreply.github.com>
2021-12-17 13:07:53 +0200

    Create gaussElimination.java

---
+++ numericalmethods/src/main/java/com/example/demo/gaussElimination.java
@@ -0,0 +1,63 @@
+
+public class gaussElimination {
+
+    public double[] JaussElimination(double Arr[][],double[] B)throws Exception
+    {
+
+        for(int i=0;i<B.length;i++)
+        {
+            //finding the pivot
+            double pivot=Math.abs(Arr[i][i]);
+            int pivotIndx=i;
+            for(int j=i+1;j<B.length;j++)
+            {
+                if(Math.abs(Arr[j][i])>pivot)
+                {
+                    pivot=Math.abs(Arr[j][i]);
+                    pivotIndx=j;
+                }
+            }
+            // change rows in coef array
+            double[] temp=Arr[i];
+            Arr[i]=Arr[pivotIndx];
+            Arr[pivotIndx]=temp;
+            //change rows in B Matrix
+            double temp2=B[i];
+            B[i]=B[pivotIndx];
+            B[pivotIndx]=temp2;
+
+            for(int k=i+1;k<B.length;k++)
+            {
+                if(Arr[i][i]==0)
+                {
+                    throw new Exception("Error");
+                }
+                double factor=Arr[k][i]/Arr[i][i];
+                B[k]=B[k]-factor*B[i];
+
+                for(int c=i;c<B.length;c++)
+                {
+                    Arr[k][c]=Arr[k][c]-factor*Arr[i][c];
+                }
+            }
+        }
+        //Back substitution
+        double[] result=new double[B.length];
+
+        for(int i=B.length-1;i>=0;i-- )
+        {
+            double sum=0;
+
+            for(int j=i+1;j<B.length;j++)
+            {
+                sum+=Arr[i][j]*result[j];
+            }
+            if(Arr[i][i]==0)
+            {
+                throw new Exception("Error");
+            }
+            result[i]=(B[i]-sum)/Arr[i][i];
+        }
+        return result;
+    }
+}
