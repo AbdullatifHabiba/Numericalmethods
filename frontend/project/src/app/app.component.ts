fa2a342f56789fa090f4b8285c6b1af7427c1648
aa <ao1228340764a@gmail.com>
2021-12-21 10:08:57 +0200

    adding front

---
+++ frontend/project/src/app/app.component.ts
@@ -0,0 +1,142 @@
+import { Component } from '@angular/core';
+import{HttpClient}from'@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project';
+  equations:string="";
+  drop:any;
+  NonDrop:any;
+  ArrOfEquations:any=[];
+  initialGuess:string="";
+  numberOfIteration:number=0;
+  RelativeError:number=0;
+  result:any;
+  Gauess:boolean=false;
+  Jordan:boolean=false;
+  Jacobi:boolean=false;
+  sediel:boolean=false;
+  LU_decomposition:boolean=false;
+  precision:number=0;
+ //////////////////////////////////////////////////////////
+  constructor(private http:HttpClient){}
+  GauessEliminationEq(equations:string[])
+  {
+    this.http.get('http://localhost:8080/method/GaussElimination',{
+      params:{
+        Matrix:equations
+      },
+      observe:'response' 
+  }).subscribe(response=>{
+    this.result=response.body; 
+   })
+  }
+  GauessJordanEq(equations:string[])
+  {
+    this.http.get('http://localhost:8080/method/GaussJordan',{
+      params:{
+        Matrix:equations
+      },
+      observe:'response' 
+  }).subscribe(response=>{
+    this.result=response.body; 
+   })
+  }
+  LU_Eq(equations:string[])
+  {
+    this.http.get('http://localhost:8080/method/LUDecomposition',{
+      params:{
+        Matrix:equations
+      },
+      observe:'response' 
+  }).subscribe(response=>{
+    this.result=response.body; 
+   })
+  }
+  jacobiEq(equations:string[],numbOfIter:number,relativeErr:number)
+  {
+    this.http.get('http://localhost:8080/method/Jacobi',{
+      params:{
+        Matrix:equations,
+        IterationNum:numbOfIter,
+        relativeError:relativeErr
+      },
+      observe:'response' 
+  }).subscribe(response=>{
+    this.result=response.body; 
+   })
+  }
+  sedeil_Eq(equations:string[],numbOfIter:number,relativeErr:number)
+  {
+    this.http.get('http://localhost:8080/method/Seidel',{
+      params:{
+        Matrix:equations,
+        IterationNum:numbOfIter,
+        relativeError:relativeErr
+      },
+      observe:'response' 
+  }).subscribe(response=>{
+    this.result=response.body; 
+   })
+  }
+
+///////////////////////////////////////////////////
+
+
+  submitForm(){
+    this.ArrOfEquations=this.equations.split(",");
+    if(this.Gauess)
+    {
+      this. GauessEliminationEq(this.ArrOfEquations);
+    }
+    else if(this.Jordan)
+    {
+      this.GauessJordanEq(this.ArrOfEquations);
+    }
+    else if(this.LU_decomposition)
+    {
+       this.LU_Eq(this.ArrOfEquations);
+    }
+    else if(this.Jacobi)
+    {
+      this. jacobiEq(this.ArrOfEquations,this.numberOfIteration,this.RelativeError);
+    }
+    else{
+         this.sedeil_Eq(this.ArrOfEquations,this.numberOfIteration,this.RelativeError);
+    }
+  }
+  dropList1()
+  {
+    this.drop=false;
+    this.NonDrop=true;
+    this.Gauess=true;
+  }
+  dropList2(){
+    this.drop=false;
+    this.NonDrop=true;
+    this.Jordan=true;
+  }
+  dropList3()
+  {
+    this.drop=false;
+    this.NonDrop=true;
+    this.LU_decomposition=true;
+  }
+  dropList4()
+  {
+    this.drop=true;
+    this.NonDrop=true;
+    this.sediel=true;
+  }
+  dropList5()
+  {
+    this.drop=true;
+    this.NonDrop=true;
+    this.Jacobi=true;
+  }
+
+}
